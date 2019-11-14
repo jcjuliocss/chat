95fe965685bf9cb8fee6e31de1aaca7be4fe5803
Julio Cesar <39426877+jcjuliocss@users.noreply.github.com>
2019-11-14 16:34:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,5 +21,5 @@ def handle_message(message):
 
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     socketio.run(app, host='0.0.0.0', port=port)
