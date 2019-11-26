1177b9d0ebcf79f96f2f01f639d08d92b1dcfbb8
Julio Cesar <julio.silva@softrh.com.br>
2019-11-26 15:43:06 -0400

    adicionando features

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """Docstring for main."""
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect
 from flask_socketio import SocketIO
 from datetime import datetime
 
@@ -7,26 +7,69 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+usuarios_conectados = []
 
-@app.route('/')
+
+@app.route("/")
+def login():
+    """."""
+    return render_template("login.html")
+
+
+@app.route("/logar", methods=['POST'])
+def logar():
+    """."""
+    dados = request.form
+    session['nome'] = dados.get("login")
+
+    return redirect("/index")
+
+
+@app.route('/index')
 def index():
     """."""
-    return render_template('index.html')
+    return render_template('index.html', nome=session['nome'])
 
 
-def mensagem_recebida():
-    """."""
-    print('Mensagem recebida')
-
-
-@socketio.on('my event')
+@socketio.on('send message')
 def handle_message(json):
     """."""
-    print('Mensagem recebida: ' + str(json))
     hora = datetime.now().strftime('%H')
     minutos = datetime.now().strftime('%M')
     json['horario'] = str(int(hora) - 3) + ':' + minutos
-    socketio.emit('message', json, callback=mensagem_recebida)
+    json['nome'] = session['nome']
+    if 'texto' in json and len(json['texto']) > 300:
+        json['texto'] = json['texto'][:300]
+    print('Mensagem recebida: ' + str(json))
+    socketio.emit('message', json)
+
+
+@socketio.on('connect message')
+def handle_connect(json):
+    """."""
+    hora = datetime.now().strftime('%H')
+    minutos = datetime.now().strftime('%M')
+    json['horario'] = str(int(hora) - 3) + ':' + minutos
+    json['nome'] = session['nome']
+    json['texto'] = ' conectado.'
+    print("Usuario conectado")
+    usuarios_conectados.append(json['nome'])
+    json['lista_usuarios'] = usuarios_conectados
+    socketio.emit('connection', json)
+
+
+@socketio.on('disconnect message')
+def handle_disconnect(json):
+    """."""
+    hora = datetime.now().strftime('%H')
+    minutos = datetime.now().strftime('%M')
+    json['horario'] = str(int(hora) - 3) + ':' + minutos
+    json['nome'] = session['nome']
+    json['texto'] = ' desconectado.'
+    print("Usuario desconectado")
+    usuarios_conectados.remove(json['nome'])
+    json['lista_usuarios'] = usuarios_conectados
+    socketio.emit('connection', json)
 
 
 if __name__ == '__main__':
