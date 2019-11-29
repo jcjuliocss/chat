80bd85e1bbc74766ed1f55e062bb7b11378d0e0b
Julio Cesar <julio.silva@softrh.com.br>
2019-11-29 11:56:16 -0400

    .

--- main.py
+++ main.py
@@ -125,6 +125,7 @@ def handle_disconnect(json):
     json['lista_usuarios'] = m
     del(session['nome'])
     socketio.emit('connection', json)
+    return redirect('/')
 
 
 def deleta_usuario(nome):
